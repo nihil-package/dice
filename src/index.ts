1a93326f368ff23daa4b73dc1d6a17d976a5ac39
NIHILncunia <nihil_ncunia@naver.com>
2024-01-18 20:11:30 +0900

    2024 0118 문제 발견해서 수정

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,13 @@ function rollDice(dice: string, rollType = 'default'): RollDiceResult {
   const sign = +newDices > 0 ? '+' : '-';
 
   if (rollType === 'min') {
-    details.push(1);
+    for (let i = 0; i < Math.abs(+newDices); i++) {
+      details.push(1);
+    }
   } else if (rollType === 'max') {
-    details.push(+sides);
+    for (let i = 0; i < Math.abs(+newDices); i++) {
+      details.push(+sides);
+    }
   } else {
     for (let i = 0; i < Math.abs(+newDices); i++) {
       const number = Math.ceil(Math.random() * +sides);
@@ -88,27 +92,10 @@ function rollAllDices(formulas: string, rollType = 'default'): RollDiceModResult
 
 const preset = [
   'D2', 'D4', 'D6', 'D8', 'D10',
-  'D12', 'D20', '2D4', '2D6', '2D8',
+  'D12', 'D20', 'D100', '2D4', '2D6', '2D8',
   '2D10', '2D12', '3D4', '3D6', '3D8',
   '3D10', '3D12',
 ];
-
-// const preset = {
-//   diceD2: () => rollDice('D2'),
-//   diceD4: () => rollDice('D4'),
-//   diceD6: () => rollDice('D6'),
-//   diceD8: () => rollDice('D8'),
-//   diceD10: () => rollDice('D10'),
-//   diceD12: () => rollDice('D12'),
-//   diceD20: () => rollDice('D20'),
-//   dice2D4: () => rollDice('2D4'),
-//   dice2D8: () => rollDice('2D8'),
-//   dice2D10: () => rollDice('2D10'),
-//   dice3D4: () => rollDice('3D4'),
-//   dice3D6: () => rollDice('3D6'),
-//   dice3D8: () => rollDice('3D8'),
-//   diceD100: () => rollDice('D100'),
-// };
 
 export {
   rollDice,
